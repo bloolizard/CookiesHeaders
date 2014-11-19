15bc757592cf16b1eee577c2102c8e2b6cfc000b
Edwin Villanueva <edwincv0@gmail.com>
2014-11-19 14:49:11 -0800

    updated readme, updated max-age on module

--- fdo.js
+++ fdo.js
@@ -2,7 +2,7 @@
 
 exports.edwin = function(req, res, next){
     res.set({
-        'Cache-Contol': 'max-age=1',
+        'Cache-Contol': 'max-age=315569800', // in seconds per W3C
         'Expires':'Tue Nov 19 2024 00:00:00 GMT-0800 (PST)' //set the Expires Header for 10 years
     });
     next();
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 ### Node.JS Experimentation
 
 Adding Cookies and Headers as a Module.
+
+####Useful Stuff
+Curl can be used to get Headers.
+'curl -I http://localhost:3000'
